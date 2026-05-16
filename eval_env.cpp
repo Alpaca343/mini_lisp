085371816169642d65a6b71e319e92d64d1cdfa3
Alpaca343 <wubigruns@outlook.com>
2026-05-16 10:36:10 +0800

    lv3

--- eval_env.cpp
+++ eval_env.cpp
@@ -1,17 +1,23 @@
 #include "eval_env.h"
+
 ValuePtr EvalEnv::eval(ValuePtr expr) {
     if (expr->isSelfEvaluating()) {
         return expr;
     } else if (expr->isNil()) {
         throw LispError("Evaluating nil is prohibited.");
-    }
-    else if (expr->isPair()) {
-        using namespace std::literals;  // 使用 s 后缀
-        std::vector<ValuePtr> v = expr->toVector();
-        if (v.size() != 3) {
-            throw LispError("define requires exactly 2 arguments");
+    } else if (auto name = expr->asSymbol()) {
+        if (auto value = this->lookup(*name)) {
+            return value;
+        } else {
+            throw LispError("Variable " + *name + " not defined.");
         }
+    } else if (expr->isPair()) {
+        using namespace std::literals; //使用 s 后缀
+        std::vector<ValuePtr> v = expr->toVector();
         if (v[0]->asSymbol() == "define"s) {
+            if (v.size() != 3) {
+                throw LispError("define requires exactly 2 arguments");
+            }
             if (auto name = v[1]->asSymbol()) {
                 ValuePtr val = eval(v[2]);
                 symbolTable[*name] = val;
@@ -22,19 +28,15 @@ ValuePtr EvalEnv::eval(ValuePtr expr) {
         } else {
             throw LispError("Unimplemented");
         }
-    }
-    else if (auto name = expr->asSymbol()) {
-        if (auto value = this->lookup(*name)) {
-            return value; 
-        } else {
-            throw LispError("Variable " + *name + " not defined.");
-        }
-    }
-    else {
+    } else {
         throw LispError("Unimplemented");
     }
 }
 
 ValuePtr EvalEnv::lookup(std::string name) {
-    return symbolTable[name];
+    auto it = symbolTable.find(name);
+    if (it != symbolTable.end()) {
+        return it->second;
+    }
+    return nullptr;
 }
